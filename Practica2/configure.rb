a5f8dd4385b4eeebf5cc8115e7afb89b9293d56d
Madelaine Martin <made_2503@hotmail.com>
2013-09-27 16:34:55 +0100

    Empezando la practica

---
+++ Practica2/configure.rb
@@ -0,0 +1,6 @@
+Twitter.configure do |config|
+  config.consumer_key = 'znZPrbyhUOBt8cztCbIc6A'
+  config.consumer_secret = 'VmLGTKW39Imnmmo3HZYkZEk9cLU8d9LDWOKRQpBXMY'
+  config.oauth_token = "246270916-7Z3leKVOoEwKUBxAWtH0C5vwLX7QWgsvOd8QqbHT"
+  config.oauth_token_secret = "3YzLOESUA73C7blbCVOBJWIU335WQoK5Qlb17ksi1w"
+end
