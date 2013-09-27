29e171bd0936f91ded7489ed4f117ee1847ec970
Madelaine Martin <made_2503@hotmail.com>
2013-09-27 19:06:28 +0100

    Modificando Formulario

--- Practica2/Formulario.rb
+++ Practica2/Formulario.rb
@@ -1,8 +1,9 @@
 require 'twitter'
 require './configure'
 require 'rack'
-require 'pry-debugger'
 require 'thin'
+require 'erb'
+require 'bootstrap'
 
 class Formulario
 	def call env
@@ -10,10 +11,16 @@ class Formulario
 		res = Rack::Response.new 
 		
 		binding.pry if ARGV[0]
-		res['Content-Type'] = 'text/html'
-		nombre = (req["name1"] && req["name1"] != '') ? req["name1"] :'made_503'
 		
+		res['Content-Type'] = 'text/html'
+		nombre = (req["name1"] && req["name1"] != '') ? req["name1"] :''
 		tweet = Twitter.user_timeline(nombre).first.text 
+		numero = req["number"] if req["number"]
+
+		tweet = Twitter.user_timeline(nombre,{:count => numero.to_i})
+		aux = tweet.length.to_i - 1
+		i = 0
+
 
 		res.write <<-"EOS"
 			<!DOCTYPE HTML>
@@ -27,13 +34,16 @@ class Formulario
 				<body>
 					<h3>
 						<form action="/" method="post">
-							Nombre usuario en Twitter: <input type="text" name="name1" value="" autofocus></br>
-							Ultimo tweet: #{tweet}
-						
-							Numero de Tweets: <input type="text" name="number" value=""></br>
+							Nombre usuario en Twitter: <input type="text" name="name1" value="" size="20" maxlength="20" autofocus></br>
+
+							Numero de Tweets: <input type="text" name="number" value="" size="10" maxlength="10"></br>
 							<input type="submit" value="Enviar">
 						</form>
 					</h3>
+					<h4> Tweets </h4>
+						for i in(0..aux)
+							</br> #{tweet.map {|i| i.text}} </br>
+						end
 				</body>
 			</html>
 			EOS
